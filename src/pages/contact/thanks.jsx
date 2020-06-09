df2d8282acf5485923b1567ffce9b22f4cbf6800
Benjamin Waterlot <waterlotbenjamin@gmail.com>
2020-06-09 21:51:37 +0200

    clean forms

--- src/pages/contact/thanks.jsx
+++ src/pages/contact/thanks.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 
-export default () => (
+const Thanks = () => (
   <Layout>
-    <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
-        </div>
+    <div className="container">
+      <div className="box py-6 px-6 content">
+        <h1>Thank you!</h1>
+        <p>This is a custom thank you page for form submissions</p>
       </div>
-    </section>
+    </div>
   </Layout>
 )
+
+export default Thanks
