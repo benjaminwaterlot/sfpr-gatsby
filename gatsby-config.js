1a8ebfbdead0a6aa6240cc29a5bba328a1dd6ac8
Benjamin Waterlot <waterlotbenjamin@gmail.com>
2020-12-08 11:29:57 +0100

    chore: upgrade deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+console.log(`>>>>> ${process.env.GA_TRACKING_ID}`)
+
 module.exports = {
   siteMetadata: {
     title: 'SFPR',
